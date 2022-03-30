80534b3b1a2112199ba96927ee981af1e81d586b
Jose Alvarado <jos.alvatorre@gmail.com>
2022-03-30 14:51:24 -0700

    overcomplicated three number sum prime

---
+++ three_number_sum_prime.py
@@ -0,0 +1,88 @@
+from typing import Callable, List, Optional, Tuple
+
+Triplet = Tuple[int, int, int]
+
+
+def median_low(first_index: int, last_index: int) -> int:
+    if last_index < first_index:
+        raise Exception("crossed indices")
+
+    return (first_index + last_index) // 2
+
+
+def search(
+    first_index: int,
+    last_index: int,
+    compare: Callable[[int], int],
+    return_closest: bool = False,
+) -> Optional[int]:
+
+    if last_index < first_index:
+        return None
+
+    while first_index < last_index:
+        med = median_low(first_index, last_index)
+        comp = compare(med)
+
+        if 0 < comp:
+            # too big so decrease
+            last_index = med - 1
+        elif comp < 0:
+            # too small so increase
+            first_index = med + 1
+        else:
+            return med
+    # first_index == last_index now
+    comp = compare(first_index)
+    if comp == 0:
+        return first_index
+
+    if return_closest:
+        return first_index
+    return None
+
+
+def compare(x: int, y: int) -> int:
+    if x < y:
+        return -1
+    if y < x:
+        return 1
+    return 0
+
+
+def threeNumberSum(
+    array: List[int],
+    target_sum: int,
+) -> List[Tuple[int, int, int]]:
+
+    array = sorted(array)
+    result: List[Triplet] = []
+
+    for a_index, a in enumerate(array):
+
+        def compare_to_largest_b(index: int):
+            x = array[index]
+            smallest_possible_sum = a + (a + 1) + x
+            return compare(smallest_possible_sum, target_sum)
+
+        largest_b_index = search(
+            a_index + 1,
+            len(array) - 1,
+            compare_to_largest_b,
+            return_closest=True,
+        )
+        if largest_b_index < a_index + 2:
+            break
+
+        for b_index in reversed(range(a_index + 1, largest_b_index + 1)):
+            b = array[b_index]
+
+            c_index = search(
+                a_index + 1,
+                b_index - 1,
+                lambda i: compare(sum((a, b, array[i])), target_sum),
+            )
+            if c_index is not None:
+                result.append((a, array[c_index], b))
+
+    return result
