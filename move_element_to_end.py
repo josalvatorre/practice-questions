8c458cb4f82fbc816a27626ade3571c3604239d1
Jose Alvarado <jos.alvatorre@gmail.com>
2022-04-02 16:30:56 -0700

    move element to end simplification

--- move_element_to_end.py
+++ move_element_to_end.py
@@ -2,21 +2,12 @@ from typing import List
 
 
 def move_element_to_end(array: List[int], to_move: int):
-    # j = where to place the next toMove we find.
+
     j = len(array) - 1
-    # traverse array in reverse order.
-    i = 0
 
-    while i < j:
-        # Avoid swapping two toMove's.
-        # Decrement j to next non-toMove value.
-        while i < j and array[j] == to_move:
-            j -= 1
-
+    for i in reversed(range(0, len(array))):
         if array[i] == to_move:
-            # Move toMove to j.
-            array[i], array[j] = array[j], array[i]
-
-        i += 1
+            array[j], array[i] = array[i], array[j]
+            j -= 1
 
     return array
