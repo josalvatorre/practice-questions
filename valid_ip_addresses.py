6bf74f59d9f8863cf4dbf39b6fcbb78abe169e74
Jose Alvarado <jos.alvatorre@gmail.com>
2022-04-02 17:28:56 -0700

    clean up valid ip addresses

--- valid_ip_addresses.py
+++ valid_ip_addresses.py
@@ -1,20 +1,23 @@
-from typing import Generator, List
+from typing import Generator, List, Tuple
 
 
 def valid_ip_addresses(
     string: str,
     start: int,
     chunks_remaining: int,
-    dot_insert_indices: List[int],
+    dot_insert_indices: Tuple[int, ...],
 ) -> Generator[str, None, None]:
+    def is_valid_chunk(start: int, end: int):
+        chunk = string[start:end]
+        chunk_val = int(chunk)
+        return (
+            len(chunk) <= 3
+            and chunk_val <= 255
+            and len(chunk) == len(str(chunk_val))
+        )
+
     if chunks_remaining <= 1:
-        last_chunk = string[start:]
-        last_chunk_int = int(last_chunk)
-        if (
-            len(last_chunk) > 3
-            or 255 < last_chunk_int
-            or (len(str(last_chunk_int)) != len(last_chunk))
-        ):
+        if not is_valid_chunk(start, len(string)):
             return
 
         result = []
@@ -32,18 +35,18 @@ def valid_ip_addresses(
     max_digit_count = min(3, (len(string) - 1) - start)
     for digit_count in range(1, max_digit_count + 1):
         new_start = start + digit_count
-        chunk = string[start:new_start]
-        number = int(chunk)
-        if 255 < number or (len(str(number)) != len(chunk)):
+
+        if not is_valid_chunk(start, new_start):
             break
+
         yield from valid_ip_addresses(
             string,
             new_start,
             chunks_remaining - 1,
-            dot_insert_indices + [new_start],
+            dot_insert_indices + (new_start,),
         )
     pass
 
 
 def valid_ip_addresses_list(string: str) -> List[int]:
-    return list(valid_ip_addresses(string, 0, 4, []))
+    return list(valid_ip_addresses(string, 0, 4, ()))
