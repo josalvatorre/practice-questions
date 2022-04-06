fc387d84c64631f6c6ddab23d292a2304831dd90
Jose Alvarado <jos.alvatorre@gmail.com>
2022-04-05 18:00:55 -0700

    improvement for group anagrams

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,27 +1,19 @@
-from itertools import islice, zip_longest
-from typing import List, Tuple
+from typing import Dict, List
 
 
 def group_anagrams(words: List[str]) -> List[List[int]]:
     if len(words) == 0:
         return []
 
-    sorted_words: List[Tuple[str, int]] = sorted(
-        ("".join(sorted(word)), i) for i, word in enumerate(words)
-    )
+    groups_by_sorted_words: Dict[str, List[str]] = {}
 
-    groups: List[List[str]] = [[]]
+    for word in words:
+        sorted_word = "".join(sorted(word))
 
-    for sw_info_1, sw_info_2 in zip_longest(
-        islice(sorted_words, 0, len(sorted_words)),
-        islice(sorted_words, 1, len(sorted_words)),
-    ):
-        group_1 = groups[-1]
+        group = groups_by_sorted_words.get(sorted_word)
+        if group is None:
+            groups_by_sorted_words[sorted_word] = [word]
+        else:
+            group.append(word)
 
-        if sw_info_2 is not None and sw_info_1[0] != sw_info_2[0]:
-            groups.append([])
-
-        group_1.append(words[sw_info_1[1]])
-        pass
-
-    return groups
+    return list(groups_by_sorted_words.values())
