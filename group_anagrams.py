0db144ec989d9992fae3f2883d1d2b66b659a4fe
Jose Alvarado <jos.alvatorre@gmail.com>
2022-04-05 18:05:08 -0700

    improvement for group anagrams

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,19 +1,17 @@
-from typing import Dict, List
+from collections import defaultdict
+from typing import DefaultDict, List
 
 
 def group_anagrams(words: List[str]) -> List[List[int]]:
     if len(words) == 0:
         return []
 
-    groups_by_sorted_words: Dict[str, List[str]] = {}
+    groups_by_sorted_words: DefaultDict[str, List[str]] = defaultdict(
+        lambda: []
+    )
 
     for word in words:
         sorted_word = "".join(sorted(word))
-
-        group = groups_by_sorted_words.get(sorted_word)
-        if group is None:
-            groups_by_sorted_words[sorted_word] = [word]
-        else:
-            group.append(word)
+        groups_by_sorted_words[sorted_word].append(word)
 
     return list(groups_by_sorted_words.values())
