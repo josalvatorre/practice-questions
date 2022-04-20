f3913864d380010d8fd8689a35bfbc84396c6363
Jose Alvarado <jos.alvatorre@gmail.com>
2022-04-20 15:35:22 -0700

    min_number_of_coins_for_change

--- min_number_of_coins_for_change.py
+++ min_number_of_coins_for_change.py
@@ -1,44 +1,20 @@
-from functools import lru_cache
-from typing import List, Optional
+from typing import List
 
 
-def min_number_of_coins_for_change(amount: int, denoms: List[int]) -> int:
+def min_number_of_coins_for_change(total: int, denoms: List[int]) -> int:
 
-    denoms.sort(reverse=True)
+    coin_counts = [float("inf")] * (total + 1)
+    coin_counts[0] = 0
 
-    def min_coins(remaining: int, start: int) -> Optional[int]:
-
-        if remaining < 0:
-            return None
-        if remaining == 0:
-            return 0
-
-        min_coin_count = None
-        for d in range(start, len(denoms)):
-            denomination = denoms[d]
-
-            for coin_count in reversed(
-                range(
-                    1,
-                    (remaining // denomination) + 1,
+    for denomination in denoms:
+        for amount in range(len(coin_counts)):
+            if denomination <= amount:
+                coin_counts[amount] = min(
+                    coin_counts[amount],
+                    1 + coin_counts[amount - denomination],
                 )
-            ):
-                result = min_coins(
-                    remaining - (denomination * coin_count),
-                    d + 1,
-                )
-                if result is not None:
-                    result += coin_count
-                    if min_coin_count is None:
-                        min_coin_count = result
-                    else:
-                        min_coin_count = min(
-                            result,
-                            min_coin_count,
-                        )
-        return min_coin_count
+        pass
 
-    coins = lru_cache(maxsize=None)(min_coins)(amount, 0)
-    if coins is None:
+    if coin_counts[-1] == float("inf"):
         return -1
-    return coins
+    return coin_counts[-1]
