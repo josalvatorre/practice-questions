c92c64bc67f8e04d438e2ce9060f58990bde0f72
Jose Alvarado <jos.alvatorre@gmail.com>
2022-04-21 11:18:39 -0700

    min_height_bst

---
+++ min_height_bst.py
@@ -0,0 +1,43 @@
+from typing import Generator, List
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
+
+    pass
+
+
+def min_height_bst(array: List[int]) -> BST:
+    def pre_order_traversal(
+        start: int,
+        end: int,
+    ) -> Generator[int, None, None]:
+        if end < start:
+            return
+
+        mid = (start + end) // 2
+        yield array[mid]
+        yield from pre_order_traversal(start, mid - 1)
+        yield from pre_order_traversal(mid + 1, end)
+        pass
+
+    elements = pre_order_traversal(0, len(array) - 1)
+    tree = BST(next(elements))
+    for e in elements:
+        tree.insert(e)
+    return tree
