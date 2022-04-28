b84473a310b19aab1b48269580bb2ae616ce8821
Jose Alvarado <jos.alvatorre@gmail.com>
2022-04-28 16:27:39 -0700

    simpler shift_linked_list

--- shift_linked_list.py
+++ shift_linked_list.py
@@ -5,7 +5,7 @@ class LinkedList:
         self.next = None
 
 
-def shift_linked_list(head: LinkedList, k: int) -> None:
+def shift_linked_list(head: LinkedList, k: int) -> LinkedList:
     """
     2
     0 -> 1 -> 2 -> 3 -> 4 -> 5
@@ -19,51 +19,22 @@ def shift_linked_list(head: LinkedList, k: int) -> None:
     0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 0
     0 -> 1    2 -> 3 -> 4 -> 5
     """
-    if k == 0:
-        return head
-
-    tail = head
     length = 1
+    tail = head
     while tail.next is not None:
         tail = tail.next
         length += 1
 
-    sign = k // abs(k)
-    k = sign * (abs(k) % length)
-
-    if k == 0:
+    offset = abs(k) % length
+    if offset == 0:
         return head
 
-    prev = head
-    if k > 0:
-        tail = head
-        for _ in range(k):
-            tail = tail.next
-            if tail is None:
-                raise Exception(f"{k = } is too large for this list")
-        # tail is now k stops ahead of prev
+    new_tail_index = (length - offset if k > 0 else offset) - 1
+    new_tail = head
+    for _ in range(new_tail_index):
+        new_tail = new_tail.next
 
-        while tail.next is not None:
-            prev = prev.next
-            tail = tail.next
-        # tail is now actually the tail
-
-        new_head = prev.next
-        prev.next = None
-        tail.next = head
-        return new_head
-    else:
-        prev = head
-        for _ in range(abs(k) - 1):
-            prev = prev.next
-
-        new_head = prev.next
-        second = new_head
-        prev.next = None
-
-        while second.next is not None:
-            second = second.next
-
-        second.next = head
-        return new_head
-    pass
+    new_head = new_tail.next
+    new_tail.next = None
+    tail.next = head
+    return new_head
