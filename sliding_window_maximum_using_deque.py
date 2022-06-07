05fbabafcdfe35a51ae1232dc539dc64d36bbe8c
Jose Alvarado <jos.alvatorre@gmail.com>
2022-06-06 22:35:53 -0700

    ugly but working sliding_window_maximum_using_deque

---
+++ sliding_window_maximum_using_deque.py
@@ -0,0 +1,35 @@
+from collections import deque
+from typing import List
+
+
+def max_sliding_window(nums: List[int], window_size: int) -> List[int]:
+    def clean_decreasing_window(index: int) -> None:
+        while (
+            0 < len(decreasing_window)
+            and decreasing_window[0] < index - window_size + 1
+        ):
+            decreasing_window.popleft()
+
+        while (
+            0 < len(decreasing_window)
+            and nums[decreasing_window[-1]] < nums[index]
+        ):
+            decreasing_window.pop()
+        pass
+
+    decreasing_window = deque()
+
+    for i in range(window_size):
+        clean_decreasing_window(i)
+        decreasing_window.append(i)
+        pass
+
+    maxs: List[int] = [nums[decreasing_window[0]]]
+
+    for i in range(window_size, len(nums)):
+        clean_decreasing_window(i)
+        decreasing_window.append(i)
+        maxs.append(nums[decreasing_window[0]])
+        pass
+
+    return maxs
