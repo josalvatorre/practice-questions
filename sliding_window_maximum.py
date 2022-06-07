357d6fb28a9c54337e7ae46e1bca297a4c5b5a09
Jose Alvarado <jos.alvatorre@gmail.com>
2022-06-06 17:29:26 -0700

    remove useless comment

--- sliding_window_maximum.py
+++ sliding_window_maximum.py
@@ -24,27 +24,6 @@ def heap_element_factory(
 
 
 def max_sliding_window(nums: List[int], window_size: int) -> List[int]:
-    """
-    1 3 -1 -3 5 3 6 7
-    0 1  2  3 4 5 6 7
-
-    [0, 2]
-    [2 0 1]
-    1 -> 3
-
-    [1, 3]
-    [2 0 1]
-    1 -> 3
-
-    [2, 4]
-    [2 5]
-    4 -> 5
-
-    [3, 5]
-    [2 5]
-    4 -> 5
-    """
-
     create_heap_element = heap_element_factory(lambda index: -nums[index])
     max_indices_heap: List[HeapElement] = []
 
