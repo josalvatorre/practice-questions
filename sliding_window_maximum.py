e00f2093d6dbe225db06400c8f644490bb205cfa
Jose Alvarado <jos.alvatorre@gmail.com>
2022-06-06 17:01:55 -0700

    semi-naive solution for sliding_window_maximum

---
+++ sliding_window_maximum.py
@@ -0,0 +1,68 @@
+import heapq
+from typing import Any, Callable, List, NamedTuple, TypeVar
+
+T = TypeVar("T")
+
+
+class HeapElement(NamedTuple):
+    x: T
+    key: Callable[[T], Any]
+
+    def __lt__(self: "HeapElement", other: "HeapElement") -> bool:
+        return self.key(self.x) < other.key(other.x)
+
+    pass
+
+
+def heap_element_factory(
+    key_function: Callable[[T], Any],
+) -> Callable[[T], HeapElement]:
+    def create_heap_element(x: T) -> HeapElement:
+        return HeapElement(x=x, key=key_function)
+
+    return create_heap_element
+
+
+def max_sliding_window(nums: List[int], window_size: int) -> List[int]:
+    """
+    1 3 -1 -3 5 3 6 7
+    0 1  2  3 4 5 6 7
+
+    [0, 2]
+    [2 0 1]
+    1 -> 3
+
+    [1, 3]
+    [2 0 1]
+    1 -> 3
+
+    [2, 4]
+    [2 5]
+    4 -> 5
+
+    [3, 5]
+    [2 5]
+    4 -> 5
+    """
+
+    create_heap_element = heap_element_factory(lambda index: -nums[index])
+    max_indices_heap: List[HeapElement] = []
+
+    for i in range(window_size):
+        heapq.heappush(max_indices_heap, create_heap_element(i))
+        pass
+
+    maxs: List[int] = [nums[max_indices_heap[0].x]]
+
+    for i in range(window_size, len(nums)):
+        window_start = i - window_size + 1
+
+        while (
+            0 < len(max_indices_heap) and max_indices_heap[0].x < window_start
+        ):
+            heapq.heappop(max_indices_heap)
+
+        heapq.heappush(max_indices_heap, create_heap_element(i))
+        maxs.append(nums[max_indices_heap[0].x])
+
+    return maxs
